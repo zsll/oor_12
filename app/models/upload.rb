7c1b91e33d44797e388402a3eae53299fdb209ac
Yangzi Liu <asu.tristan@gmail.com>
2014-01-02 15:10:28 -0800

    try using dev s3, confirmed dev works, back to production

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -16,10 +16,10 @@ class Upload < ActiveRecord::Base
   
   def self.copy_and_delete(paperclip_file_path, raw_source, delete = true)
     s3 = AWS::S3.new #create new s3 object
-    destination = s3.buckets["#{ENV['S3_BUCKET_NAME']}-development"].objects[paperclip_file_path]
+    destination = s3.buckets["#{ENV['S3_BUCKET_NAME']}-#{Rails.env}"].objects[paperclip_file_path]
     sub_source = CGI.unescape(raw_source)
     sub_source.slice!(0) # the file_file_path ends up adding an extra "/" in the beginning. We've removed this.
-    source = s3.buckets["#{ENV['S3_BUCKET_NAME']}-development"].objects["#{sub_source}"]
+    source = s3.buckets["#{ENV['S3_BUCKET_NAME']}-#{Rails.env}"].objects["#{sub_source}"]
     source.copy_to(destination, :acl => "public-read") #copy_to is a method originating from the aws-sdk gem.
     if delete
       source.delete #delete temp file.
