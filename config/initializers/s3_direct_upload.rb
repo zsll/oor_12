d30bf7b663317cf58af1eb067d8ea83e8401fdf5
Yangzi Liu <asu.tristan@gmail.com>
2014-01-02 16:02:39 -0800

    added region to s3_direct_upload.rb

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -2,6 +2,6 @@ S3DirectUpload.config do |c|
   c.access_key_id     = ENV['AWS_ACCESS_KEY_ID']
   c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   c.bucket            = "#{ENV['S3_BUCKET_NAME']}-#{Rails.env}"
-  c.region            = "s3"
+  c.region            = "s3-eu-west-1"  #This is required or it will cause https://github.com/waynehoover/s3_direct_upload/issues/30
   c.url = "https://#{c.bucket}.s3.amazonaws.com/"
 end
